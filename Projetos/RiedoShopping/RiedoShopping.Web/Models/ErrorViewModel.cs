9e7f5ff34f5f36a31e8cc1272c6c9ca6b2435cbc
VitorRiedo <vitorriedo@gmail.com>
2022-02-17 20:18:42 -0300

    RiedoShopping.Web

---
+++ Projetos/RiedoShopping/RiedoShopping.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RiedoShopping.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
